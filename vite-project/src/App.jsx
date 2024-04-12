460fe16086da7d8167f82ffb992bfcf628a23462
Tanish970 <rohith.guntur@gmail.com>
2024-04-12 20:55:59 +0000

    init project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import "./App.css";
+import Product from "/workspaces/react_new/vite-project/src/Product.jsx";
+
+function Title(){
+  return <h1>I am the title!</h1>;
+}
+
+
+function App() {
+  return <div>
+    <Product />
+    </div>
+   
+}
+
+export default App
---
+++ vite-project/src/Product.jsx
@@ -0,0 +1,8 @@
+function Product(){
+    return <div>
+        <h1>This is title</h1>
+        <h3>This is discription</h3>
+    </div>
+}
+
+export default Product
